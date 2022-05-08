8554ae2d38edc7a9ffb8dcdee596641425cf251f
barbosa92 <alexrp_92@hotmail.com>
2022-05-08 22:16:36 +0000

    Scrollable divs added|Next->Fetching

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
 			<a className="navbar-brand" href="#">Navbar</a>
 				
-			<ul className="navbar-nav">
-				<li className="nav-item dropdown">
-						<a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Dropdown link
-						</a>
-					<div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-						<a className="dropdown-item" href="#">Action</a>
-						<a className="dropdown-item" href="#">Another action</a>
-						<a className="dropdown-item" href="#">Something else here</a>
-					</div>
-				</li>
-			</ul>
+				<Dropdown>
+					<Dropdown.Toggle variant="success" id="dropdown-basic">
+						Dropdown Button
+					</Dropdown.Toggle>
+
+					<Dropdown.Menu>
+						<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+						<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+						<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
 		</nav>
 		
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,26 +2,73 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-const Home = () => {
-	return (<div className="container">
+// const Home = () => {
+// 	return (<div classNameName="container">
   
-				{/* <h1 style={{text-align:"center", color:"green"}}>GeeksforGeeks</h1> */}
-				<h2 style={{ padding: "10px 20px", textAlign: "center", color: "white"}}>ToDo</h2>
-				<h3>
-					To make horizontal scrollable in a bootstrap row?
-				</h3>
-				<div className="container horizontal-scrollable">
-					<div className="row text-center">
-						<div className="col-xs-4">1</div>
-						<div className="col-xs-4">2</div>
-						<div className="col-xs-4">3</div>
-						<div className="col-xs-4">4</div>
-						<div className="col-xs-4">5</div>
-						<div className="col-xs-4">6</div>
-						<div className="col-xs-4">7</div>
-					</div>
-				</div>
-			</div>)
-};
+// 				{/* <h1 style={{text-align:"center", color:"green"}}>GeeksforGeeks</h1> */}
+// 				<h2 style={{ padding: "10px 20px", textAlign: "center", color: "white"}}>ToDo</h2>
+// 				<h3>
+// 					To make horizontal scrollable in a bootstrap row?
+// 				</h3>
+// 				<div classNameName="container horizontal-scrollable">
+// 					<div classNameName="row text-center">
+// 						<div classNameName="col-xs-4">1</div>
+// 						<div classNameName="col-xs-4">2</div>
+// 						<div classNameName="col-xs-4">3</div>
+// 						<div classNameName="col-xs-4">4</div>
+// 						<div classNameName="col-xs-4">5</div>
+// 						<div classNameName="col-xs-4">6</div>
+// 						<div classNameName="col-xs-4">7</div>
+// 					</div>
+// 				</div>
+// 			</div>)
+// };
 
-export default Home;
+// export default Home;
+const Card= () =>{
+	return <div className="card" style={{width: "18rem;"}}>
+	<img className="card-img-top" src="..." alt="Card image cap"></img>
+	<div className="card-body">
+	  <h5 className="card-title">Card title</h5>
+	  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	  <a href="#" className="btn btn-primary">Go somewhere</a>
+	</div>
+  </div>
+}
+
+export default function App() {
+  return (
+    <div className="App">
+
+      	<div className="characters">
+			<h1>Characters</h1>
+			<div className="scrollable-div">
+				<Card/>
+				<Card/>
+				<Card/>
+			</div>
+	  	</div>
+
+		<div className="planets">
+			<h1>Planets</h1>
+			<div className="scrollable-div">
+				<Card/>
+				<Card/>
+				<Card/>
+			</div>
+	  	</div>
+		
+		<div className="Ships">
+			<h1>Ships</h1>
+			<div className="scrollable-div">
+				<Card/>
+				<Card/>
+				<Card/>
+			</div>
+	  	</div>
+		  
+    </div>
+  );
+}
+
+
